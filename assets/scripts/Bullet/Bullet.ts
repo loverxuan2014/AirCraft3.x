1ab4d6d324b9503b7c89842c740b02a0c1a49861
loverxuan2014 <loverxuan@foxmail.com>
2024-04-25 21:13:20 +0800

    修改

---
+++ assets/scripts/Bullet/Bullet.ts
@@ -0,0 +1,18 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Bullet')
+export class Bullet extends Component {
+    @property
+    BulletMoveSpeed: number = 500;
+    //start() {}
+
+    update(deltaTime: number) {
+        let newX: number = this.node.position.x;
+        let newY: number = this.node.position.y + this.BulletMoveSpeed * deltaTime;
+        let newZ: number = this.node.position.z;
+        this.node.setPosition(newX, newY, newZ);
+    }
+}
+
+
--- assets/scripts/Role/Player.ts
+++ assets/scripts/Role/Player.ts
@@ -1,17 +1,34 @@
-import { _decorator, Component, Node, NodeEventType, v3} from 'cc';
+import { _decorator, Component, Node, NodeEventType, v3, Prefab, EventTouch} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
+    @property(Prefab)
+    private bulletLv1_Pre = null;
+/*  
+    @property(Prefab)
+    private bulletLv2_Pre = null;
+    @property(Prefab)
+    private bulletLv3_pre = null;
+    @property(Prefab)
+    private bulletLv4_pre = null;
+*/
+    bulletShootCD: number = 0.5;
+
     start() {
-        this.node.on(NodeEventType.TOUCH_MOVE, (touchPos) => {
+        //控制飞机移动
+        this.node.on(NodeEventType.TOUCH_MOVE, (touchPos: EventTouch) => {
             this.node.setWorldPosition(
                 v3(touchPos.getLocation().x, touchPos.getLocation().y)
             );
         });
+        //子弹发射
+        this.schedule(() => {
+            let playerBullet: Node = this.bulletLv1_Pre;
+            playerBullet.setParent(this.node.parent);
+            playerBullet.setPosition(v3(this.node.getPosition().x, this.node.getPosition().y + 50));
+        },this.bulletShootCD);
     }
 
-    update(deltaTime: number) {
-        
-    }
+    //update(deltaTime: number) {}
 }
